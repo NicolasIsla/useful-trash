7b96e5444134fe97ad9d350612621e32183a0589
Nicolas Isla <nico.isla1@gmail.com>
2021-03-18 23:36:57 -0300

    Python graficos
    estetica, areas, subgraficos

---
+++ GRAFICOS.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+variable=np.arange(1,10,1)
+funcion=variable*2
+with plt.style.context("bmh"):
+	fig, ax = plt.subplots(figsize=(7,3))
+	#fig, (ax,ay) = plt.subplots(2,1,figsize=(7,3))
+	
+
+	plt.ylabel(" ")
+	plt.xlabel(" ")
+	plt.suptitle(" ")
+	#       areas       
+	#verde: #aae6b1
+	#azul: #999de0
+	#rojo: #e6aab5
+	#celeste: #a8e6e3
+	#amarillo: #e8e2a9
+	ax.fill_between(variable[0:4],funcion[0:4],color="#999de0")
+	ax.plot(variable, funcion, 'o--b', label=" ")
+	
+	ax.legend(loc="lower left")
+plt.show()
